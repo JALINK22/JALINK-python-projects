2cb473ce97be3b65057d1d9b656bc0d2cc9c2103
JALINK22 <133610942+JALINK22@users.noreply.github.com>
2023-05-14 21:06:37 -0400

    Add files via upload

---
+++ Operators_String.Concentration_and_Control.Flow.py
@@ -0,0 +1,40 @@
+
+
+day = str(20)
+month = "June" 
+year = str(2001)
+
+my_birthday = month + " " + day +"," + " " + year
+
+print(my_birthday)
+
+
+
+
+
+first = "happy".upper()
+
+second = "birthday".upper()
+
+third = "to".upper()
+
+fourth = "you".upper()
+
+
+final = first + " " + second + " " + third + " " + fourth + "."
+
+print (final)
+
+
+age = 21 
+
+if age < 10: 
+        print("Not permitted")
+elif age < 15: 
+            print("Permitted with a parent")
+elif age < 18: 
+            print("Permitted with anyone over 18")
+elif age > 18: 
+            print("Permitted to attend alone")
+
+
